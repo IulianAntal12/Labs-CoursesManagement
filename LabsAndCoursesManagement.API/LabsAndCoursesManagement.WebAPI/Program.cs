da0b5e0df84d330ed48b4612d40231fe35ce53fe
Iulian <iulian.antal@centric.eu>
2022-11-08 15:17:08 +0200

    database setup done

--- LabsAndCoursesManagement.API/LabsAndCoursesManagement.WebAPI/Program.cs
+++ LabsAndCoursesManagement.API/LabsAndCoursesManagement.WebAPI/Program.cs
@@ -16,6 +16,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+           .AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
